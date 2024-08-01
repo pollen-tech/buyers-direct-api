a5fd292127c5c240203849b4c1c5960fe0a4f808
raj-pollen <rajesh@pollen.tech>
2024-08-01 14:14:43 +0800

    feat: admin notification email api created when user onboarded (#6)

---
+++ src/app/notify/dto/email.dto.ts
@@ -0,0 +1,14 @@
+export class EmailAdminOnboardNotifyDto {
+    email_type: string = null;
+    contactName: string;
+    companyName: string;
+    companyType: string;
+    companyLocation: string;
+    contactEmail: string;
+    contactPhoneNumber: string;
+    contactCategoryOfInterest: string;
+    contactTargetResaleMarketCountries: string;
+    contactTargetResaleMarketCities: string;
+    monthlyOrderVolume: string;
+    adminLink: 'https://admin-dev.pollen.tech';
+}
--- src/app/onboard/domain/company.onboard.notify.service.ts
+++ src/app/onboard/domain/company.onboard.notify.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { CompanyRepository } from '../repositories/company.repository';
-import { CompanyTypeRepository } from '../repositories/company.type.repository';
-import { InterestCategoryRepository } from '../repositories/interest.category.repository';
-import { TargetMarketRepository } from '../repositories/target.market.repository';
-import { ImportMarketRepository } from '../repositories/import.market.repository';
-import { UserDto } from '../../../common/dtos/user.dto';
+import {Injectable} from '@nestjs/common';
+import {CompanyRepository} from '../repositories/company.repository';
+import {CompanyTypeRepository} from '../repositories/company.type.repository';
+import {InterestCategoryRepository} from '../repositories/interest.category.repository';
+import {TargetMarketRepository} from '../repositories/target.market.repository';
+import {ImportMarketRepository} from '../repositories/import.market.repository';
+import {UserDto} from '../../../common/dtos/user.dto';
+import {EmailNotificationService} from "../../notify/domain/email.notification.service";
+import {EmailAdminOnboardNotifyDto} from "../../notify/dto/email.dto";
+import {CompanyEntity} from "../repositories/company.entity";
+import {CompanyTypeEntity} from "../repositories/company.type.entity";
 
 @Injectable()
 export class CompanyOnboardNotifyService {
@@ -14,19 +18,21 @@ export class CompanyOnboardNotifyService {
         private interestCategoryRepository: InterestCategoryRepository,
         private targetMarketRepository: TargetMarketRepository,
         private importMarketRepository: ImportMarketRepository,
-    ) {}
+        private emailNotificationService: EmailNotificationService,
+    ) {
+    }
 
     async notifyAdmin(company_id: string, user: UserDto) {
-        const company = await this.companyRepository.findOneBy({ id: company_id });
+        const company = await this.companyRepository.findOneBy({id: company_id});
         const company_type = await this.companyTypeRepository.findById(company.company_type_id);
-        const targetMarketEntities = await this.targetMarketRepository.findBy({ company_id: company_id });
+        const targetMarketEntities = await this.targetMarketRepository.findBy({company_id: company_id});
 
-        const importMarkets = await this.importMarketRepository.findBy({ company_id: company_id }).then((results) => {
+        const importMarkets = await this.importMarketRepository.findBy({company_id: company_id}).then((results) => {
             return results.map((market) => market.country_name).join(', ');
         });
 
         const interest_categories = await this.interestCategoryRepository
-            .findBy({ company_id: company_id })
+            .findBy({company_id: company_id})
             .then((results) => results.map((result) => result.category_name).join(','));
 
         const target_countries = targetMarketEntities.map((market) => market.country_name).join(', ');
@@ -38,12 +44,13 @@ export class CompanyOnboardNotifyService {
             .join(',');
 
         const dto = this.createDto(company, company_type, user, interest_categories, target_countries, target_cities);
-
-        console.log('Notify DTO', dto);
+        return await this.emailNotificationService.sendAdminOnboardNotifyEmailForDirect(dto);
     }
 
-    private createDto(company, company_type, user: UserDto, interest_categories, target_countries, target_cities) {
-        return {
+    private createDto(company: CompanyEntity, company_type: CompanyTypeEntity, user: UserDto,
+                      interest_categories: string, target_countries: string, target_cities: string) {
+        const dto: EmailAdminOnboardNotifyDto = {
+            email_type: null,
             companyName: company.name,
             companyType: company_type.name,
             companyLocation: company.country_name,
@@ -56,5 +63,6 @@ export class CompanyOnboardNotifyService {
             contactPhoneNumber: '+' + user.country_code + user.phone_no,
             adminLink: 'https://admin-dev.pollen.tech',
         };
+        return dto;
     }
 }
